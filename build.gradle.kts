3641186f0a133c54e315591b6169c0d772b46df2
Christian Meyer <christian.meyer@gmail.com>
2020-11-28 12:55:28 +0100

    updated build file: added more dependencies, bumped kotlin version, added more kotlin compiler options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.4.10"
-    id("org.jetbrains.kotlin.kapt") version "1.4.10"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
+    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    id("org.jetbrains.kotlin.kapt") version "1.4.20"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.20"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("io.micronaut.application") version "1.2.0"
 }
@@ -9,7 +9,7 @@ plugins {
 version = "0.1"
 group = "com.github.chrisime"
 
-val kotlinVersion=project.properties.get("kotlinVersion")
+val kotlinVersion = project.properties["kotlinVersion"]
 repositories {
     mavenCentral()
     jcenter()
@@ -28,10 +28,15 @@ dependencies {
     implementation("io.micronaut:micronaut-validation")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+
     implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
-    implementation("io.micronaut:micronaut-runtime")
-    implementation("javax.annotation:javax.annotation-api")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
     implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.rxjava3:micronaut-rxjava3:1.1.0")
+
+    implementation("javax.annotation:javax.annotation-api")
+
     runtimeOnly("ch.qos.logback:logback-classic")
     runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
@@ -42,18 +47,32 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.toVersion("11")
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
     compileKotlin {
         kotlinOptions {
             jvmTarget = "11"
+            freeCompilerArgs = listOf(
+                    "-Xjsr305=strict",
+                    "-Xstrict-java-nullability-assertions",
+                    "-Xuse-experimental=kotlin.Experimental"
+            )
+            apiVersion = "1.4"
+            languageVersion = "1.4"
         }
     }
     compileTestKotlin {
         kotlinOptions {
             jvmTarget = "11"
+            freeCompilerArgs = listOf(
+                    "-Xjsr305=strict",
+                    "-Xstrict-java-nullability-assertions",
+                    "-Xuse-experimental=kotlin.Experimental"
+            )
+            apiVersion = "1.4"
+            languageVersion = "1.4"
         }
     }
 
